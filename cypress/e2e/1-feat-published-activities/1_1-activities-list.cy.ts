e06c7d85f966db4e42f217e02bcc40e37b9f496b
Alberto Basalo <albertobasalo@hotmail.com>
2023-06-14 14:28:04 +0200

    feat: 1 1 activity list

--- cypress/e2e/1-feat-published-activities/1_1-activities-list.cy.ts
+++ cypress/e2e/1-feat-published-activities/1_1-activities-list.cy.ts
@@ -1,36 +1,67 @@
 /**
- * Given the Activities list
- *   when the page is loaded with the published activities
+ * Given the Published Activities list
+ *   when the Home page is loaded
  *    then should show the number of activities
- *    then should show activities name, price, and date
  *    then should have a link to the activity page
  *    then should list in a monospace font
- *    then should only show published activities
+ *    then should show activities name, price, and date
  */
 
-describe("Given the Activities list", () => {
+describe("Given the Published Activities list", () => {
   const PAGE_URL = "/";
   const API_URL = `${Cypress.env("apiUrl")}/activities?state=published`;
   const LIST_SELECTOR = "main[name='list-content']";
   let publishedActivities: any[] = [];
   let activitiesCounter = 0;
   let firstActivity: any = null;
-  context("when the page is loaded with the published activities", () => {
+  beforeEach(() => {
+    cy.fixture("activities").then((fixtureContent) => {
+      const activities = fixtureContent as unknown as any[];
+      publishedActivities = activities.filter((activity: any) => activity.state === "published");
+      activitiesCounter = publishedActivities.length;
+      firstActivity = publishedActivities[0];
+      cy.intercept("GET", API_URL, {
+        body: publishedActivities,
+      });
+    });
+  });
+  context("when the page is loaded", () => {
     beforeEach(() => {
       cy.visit(PAGE_URL);
-      cy.fixture("activities").then((fixtureContent) => {
-        const activities = fixtureContent as unknown as any[];
-        publishedActivities = activities.filter((activity: any) => activity.state === "published");
-        activitiesCounter = publishedActivities.length;
-        firstActivity = publishedActivities[0];
-        cy.intercept("GET", API_URL, {
-          body: publishedActivities,
-        });
-      });
+      cy.get(`${LIST_SELECTOR} div`).as("activityItems");
     });
     it("then should show the number of activities", () => {
       cy.get("[name='items-count']").should("contain.text", activitiesCounter);
       cy.get("[name='activity-item']").should("have.length", activitiesCounter);
+    });
+
+    it("then should have a link to the activity page", () => {
+      // cy.get(`main[name='list-content'] div:nth-child(1) > [name='title'] a`).should(
+      //   "have.attr",
+      //   "href",
+      //   `/activities/${firstActivity.slug}`
+      // );
+      // cy.get(`${LIST_SELECTOR} div:nth-child(1) > [name='title'] a`).should(
+      //   "have.attr",
+      //   "href",
+      //   `/activities/${firstActivity.slug}`
+      // );
+      cy.get(`${LIST_SELECTOR} div`)
+        .first()
+        .children(`[name=title]`)
+        .children("a")
+        .should("have.attr", "href", `/activities/${firstActivity.slug}`);
+    });
+    it("then should list in a monospace font", () => {
+      // cy.get(`${LIST_SELECTOR} div:nth-child(1) > [name='title']`).should(
+      //   "have.css",
+      //   "font-family",
+      //   "monospace"
+      // );
+      cy.get("@activityItems")
+        .first()
+        .children(`[name='title']`)
+        .should("have.css", "font-family", "monospace");
     });
     it("then should show activities name, price, and date", () => {
       cy.get(`#${firstActivity.slug}`).then((firstActivityElement) => {
@@ -39,27 +70,24 @@ describe("Given the Activities list", () => {
           firstActivity.price
         );
         const printedDate = firstActivityElement.find("time").text();
-        const actual = new Date(printedDate).toLocaleDateString();
-        const expected = new Date(firstActivity.date).toLocaleDateString();
+        const actual = new Date(printedDate).getFullYear();
+        const expected = new Date(firstActivity.date).getFullYear();
         expect(actual).to.equals(expected);
       });
     });
-    it("then should have a link to the activity page", () => {
-      cy.get(`${LIST_SELECTOR} div:nth-child(1) > [name='title'] a`).should(
-        "have.attr",
-        "href",
-        `/activities/${firstActivity.slug}`
-      );
-    });
-    it("then should list in a monospace font", () => {
-      cy.get(`${LIST_SELECTOR} div:nth-child(1) > [name='title']`).should(
-        "have.css",
-        "font-family",
-        "monospace"
-      );
-    });
-    it("then should only show published activities", () => {
-      cy.get(`${LIST_SELECTOR} div`).should("not.contain.text", "draft");
+    it("THEN should show activities name, price, and date", () => {
+      cy.get(`#${firstActivity.slug}`).as("firstActivityElement");
+      cy.get("@firstActivityElement")
+        .children('[name="title"]') // act
+        .contains(firstActivity.title); // assert
+      cy.get("@firstActivityElement")
+        .find('[data-itemprop="priceCurrency"]') // act
+        .contains(firstActivity.price); // assert
+      const expected = new Date(firstActivity.date).getFullYear();
+      cy.get("@firstActivityElement")
+        .find("time")
+        .invoke("text") // like a sub query
+        .should("contains", expected);
     });
   });
 });
