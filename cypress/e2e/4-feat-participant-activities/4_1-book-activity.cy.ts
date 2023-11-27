94a7369d61bdd6744807a5b8bd8770248acae25d
Alberto Basalo <albertobasalo@hotmail.com>
2023-11-27 13:10:09 +0100

    feat: page object

--- cypress/e2e/4-feat-participant-activities/4_1-book-activity.cy.ts
+++ cypress/e2e/4-feat-participant-activities/4_1-book-activity.cy.ts
@@ -1,29 +1,28 @@
+// interceptors with fixtures
+
 /**
  * Given I am a valid logged-in user visiting the first published activity page
  *  When I click on the "Book" button
  *   Then I should see a confirmation message
  */
-
 describe("Given I am a valid logged-in user visiting the first published activity page", () => {
-  const PAGE_URL = "http://localhost:4200/";
-
   beforeEach(() => {
     // Arrange
+    const postBookingUrl = `${Cypress.env("apiUrl")}/bookings/**`;
+    const response = { statusCode: 201 };
+    cy.intercept("POST", postBookingUrl, response).as("postBooking");
     cy.login();
-    cy.interceptPublishedActivities();
-    cy.interceptFirstActivity();
-    cy.interceptPostBooking();
-    cy.visit(PAGE_URL);
-    cy.get(`[name='list-content'] a`).first().click();
+    cy.visit("/activities");
+    cy.get("#activities-list").find("li a").first().click();
   });
   context("When I click on the 'Book' button", () => {
     beforeEach(() => {
       // Act
-      cy.get(`#book-activity`).click();
+      cy.get("button").click();
     });
     it("Then I should see a confirmation message", () => {
       // Assert
-      cy.get("#booked-activity-dialog").should("contain", "Activity Booked");
+      cy.get("h3").should("contain", "Booking successfully done");
     });
   });
 });
--- cypress/e2e/4-feat-participant-activities/4_2-view-my-activities.cy.ts
+++ cypress/e2e/4-feat-participant-activities/4_2-view-my-activities.cy.ts
@@ -1,7 +1,29 @@
-/**
- * Given
- *  When
- *   Then
- */
+// intercept responses with fixtures
 
-// To Do: Student exercise
+/**
+ * Given I am a valid logged-in user
+ *  When visiting my profile page
+ *   Then I should see my bookings
+ */
+describe("Given I am a valid logged-in user", () => {
+  const userId = "1";
+  const url = `${Cypress.env("apiUrl")}/bookings?userId=${userId}`;
+  beforeEach(() => {
+    // Arrange
+    cy.login();
+    cy.intercept("GET", url, { fixture: "bookings" }).as("getBookings");
+  });
+  context("When visiting my profile page", () => {
+    beforeEach(() => {
+      // Act
+      cy.visit("/auth/profile");
+    });
+    it("Then I should see my bookings", () => {
+      // Assert
+      cy.wait("@getBookings").then((interception) => {
+        const bodyLength = interception.response?.body.length || 0;
+        cy.get("article.booking").should("have.length", bodyLength);
+      });
+    });
+  });
+});
